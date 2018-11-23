690013782f58f8202d5de627eb6ef0b2e9817ff5
Rand Tarek <rand.tarek@gmail.com>
2018-11-23 20:11:07 +0200

    added background and renamed assets

--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import Weather from './../Weather/Weather';
+import './Homepage.css';
 
 class Homepage extends Component {
     render() {
       return (
-        <div>
-            <h1>This is our new Homepage</h1>
+        <div id="background">
+            {/* <h1>This is our new Homepage</h1> */}
             <Weather />
         </div>
       );
