7604a0fb3613182be8b1b140890f044b9ff593ba
Rand Tarek <rand.tarek@gmail.com>
2018-11-23 15:09:57 +0200

    created Homepage and rendered in App

---
+++ src/Homepage/Homepage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Homepage extends Component {
+    render() {
+      return (
+        <div>
+            <h1>This is our new Homepage</h1>
+        </div>
+      );
+    }
+  }
+  
+  export default Homepage;
