f46ed8189dfb97d30379a1875771dac98811ca90
Rand Tarek <rand.tarek@gmail.com>
2018-11-23 17:49:33 +0200

    added assets

--- src/Components/Weather/Weather.js
+++ src/Components/Weather/Weather.js
@@ -6,21 +6,14 @@ class Weather extends Component {
     constructor() {
         super();
         this.state = {
-            temp: '',
+            temperature: undefined,
+            description: undefined,
+            pressure: undefined,
+            humidity: undefined,
+            city: undefined,
+            country: undefined,
         };
     }
-
-    // componentDidMount() {
-    //     const weatherAPI = '49f5f1b2aa04f7d7e6436cbe2ed83bfc';
-
-    //     fetch('http://api.openweathermap.org/data/2.5/weather?q=Cairo&APPID=' + weatherAPI + '&units=metric&mode=json')
-    //     .then(result => {
-    //         return result.json();
-    //     }).then(data => {
-    //         this.setState({ temp: data.result.main.temp });
-    //         console.log(this.state.temp);
-    //     });
-    // }
 
     getWeather = async () => {
         const APICall = await fetch('http://api.openweathermap.org/data/2.5/weather?q=Cairo&APPID=' + weatherAPI + '&units=metric');
