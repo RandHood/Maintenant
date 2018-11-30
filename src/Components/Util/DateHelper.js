41bf7010e52d2314c828f7f77d891c514c8e300e
Rand Tarek <rand.tarek@gmail.com>
2018-11-30 23:46:57 +0200

    added icons and attempt for display

--- src/Components/Util/DateHelper.js
+++ src/Components/Util/DateHelper.js
@@ -1,8 +1,13 @@
-const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+const days = ['SUNDAY', 'MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY'];
+const daysSmall = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
 const months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
 
 export function getDayName(date) {
     return days[date.getDay()];
+}
+
+export function getDaySmallName(date) {
+    return daysSmall[date.getDay()];
 }
 
 export function getMonthName(date) {
--- src/Components/Weather/Weather.js
+++ src/Components/Weather/Weather.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Config from './../../local.config.json';
-import { getDayName, getMonthName } from './../Util/DateHelper';
+import { getDayName, getDaySmallName } from './../Util/DateHelper';
 import './Weather.css';
 
 const weatherProvider = Config.weather.provider;
@@ -75,8 +75,26 @@ class Weather extends Component {
         this.saveFetchedData(currentResponse, forecastResponse);
     }
 
+    getWeatherIcon(weatherDescription) {
+        let icon;
+        switch (weatherDescription) {
+            case 'Clear': icon = './../../assets/weather-icons/clear.png'; break;
+            case 'Clouds': icon = './../../assets/weather-icons/clouds.png'; break;
+            case 'Rain': icon = './../../assets/weather-icons/rain.png'; break;
+            case 'Snow': icon = './../../assets/weather-icons/snow.png'; break;
+            case 'Storm': icon = './../../assets/weather-icons/storm.png'; break;
+            default: icon = './../../assets/weather-icons/clear.png'; break;
+        }
+        return icon;
+    }
+
     saveFetchedData(currentResponse, forecastResponse) {
         const forecast = [];
+
+        // for (let i = 0; i < 40; i++) {
+        //     console.log(forecastResponse.list[i].weather[0].main + ', ' + forecastResponse.list[i].weather[0].description);
+        // }
+
         for (let i = 8; i <= 32; i += 8) {
             forecast.push({
                 date: new Date(forecastResponse.list[i].dt_txt),
@@ -85,7 +103,7 @@ class Weather extends Component {
                 pressure: forecastResponse.list[i].main.pressure,
                 humidity: forecastResponse.list[i].main.humidity,
                 windSpeed: forecastResponse.list[i].wind.speed,
-                icon: forecastResponse.list[i].weather[0].icon,
+                icon: this.getWeatherIcon(forecastResponse.list[i].weather[0].main),
             });
         }
 
@@ -97,12 +115,12 @@ class Weather extends Component {
                 pressure: currentResponse.main.pressure,
                 humidity: currentResponse.main.humidity,
                 windSpeed: currentResponse.wind.speed,
-                icon: currentResponse.weather[0].icon,
+                icon: this.getWeatherIcon(currentResponse.weather[0].main),
             },
             forecast,
         });
 
-        // console.log(this.state);
+        console.log(this.state);
         // console.log((new Date().getDate()));
         // console.log(getDayName(new Date()));
         // console.log(getMonthName(new Date()));
@@ -112,7 +130,7 @@ class Weather extends Component {
     render() {
         if (this.state.forecast[3].icon === undefined) {
             return (
-                <h1>loading</h1>
+                <h1 className="center">...</h1>
             );
         } else {
             const day_1 = this.state.current;
@@ -120,33 +138,47 @@ class Weather extends Component {
             const day_3 = this.state.forecast[1];
             const day_4 = this.state.forecast[2];
             const day_5 = this.state.forecast[3];
+            console.log(day_3.icon);
             return(
                 <div className="container">
                     <div id="day-1">
                         <div className="center">
-                            <h1>{ day_1.temperature }&deg;</h1>
-                            <br/>
-                            <h1>{ getDayName(day_1.date) }</h1>
+                            <div>
+                                <span id="day-1-temp">{ day_1.temperature }
+                                    <span className="degree">&deg;</span>
+                                </span>
+                            </div>
+                            <div>
+                                <span className="date">{ getDayName(day_1.date) + ' ' + day_1.date.getDate() }</span>
+                            </div>
                         </div>
                     </div>
                     <div className="followingDay">
                         <div className="center">
-                            <h1>{ day_2.temperature }&deg;</h1>
+                            <span className="date">{ getDaySmallName(day_2.date) }</span>
+                            <img className={day_2.description} src={day_2.icon} alt={day_2.description}/>
+                            <span className="followingDays-temp">{ day_2.temperature }&deg;</span>
                         </div>
                     </div>
                     <div className="followingDay">
                         <div className="center">
-                            <h1>{ day_3.temperature }&deg;</h1>
+                            <span className="date">{ getDaySmallName(day_3.date) }</span>
+                            <img src={day_3.icon} alt={day_3.description}/>
+                            <span className="followingDays-temp">{ day_3.temperature }&deg;</span>
                         </div>
                     </div>
                     <div className="followingDay">
                         <div className="center">
-                            <h1>{ day_4.temperature }&deg;</h1>
+                            <span className="date">{ getDaySmallName(day_4.date) }</span>
+                            <img src={day_4.icon} alt={day_4.description}></img>
+                            <span className="followingDays-temp">{ day_4.temperature }&deg;</span>
                         </div>
                     </div>
                     <div className="followingDay">
                         <div className="center">
-                            <h1>{ day_5.temperature }&deg;</h1>
+                            <span className="date">{ getDaySmallName(day_5.date) }</span>
+                            <img src={day_5.icon} alt={day_5.description}/>
+                            <span className="followingDays-temp">{ day_5.temperature }&deg;</span>
                         </div>
                     </div>
                 </div>
